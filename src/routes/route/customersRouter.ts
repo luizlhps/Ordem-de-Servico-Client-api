9f74b82c647faa05639cb08ddceaca43eb121b94
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2023-08-02 16:41:36 -0300

    feat customerRouter implments auth and permission

--- src/routes/route/customersRouter.ts
+++ src/routes/route/customersRouter.ts
@@ -1,11 +1,36 @@
 import { Router } from "express";
 import { customerController } from "../../controllers/costumerController";
+import { authPermissionVerify } from "../../controllers/authPermissionVerify";
+import { auth } from "../../controllers/authController";
 
 export const customerRouter = Router();
 
 customerRouter.get("/", customerController.getAll);
 
-customerRouter.post("/", customerController.create);
-customerRouter.delete("/:id", customerController.delete);
-customerRouter.put("/:id", customerController.update);
-customerRouter.get("/:id", customerController.getById);
+customerRouter.post(
+  "/",
+  auth.autheticate,
+  authPermissionVerify.create({ parameter: "customer" }),
+  customerController.create
+);
+
+customerRouter.delete(
+  "/:id",
+  auth.autheticate,
+  authPermissionVerify.create({ parameter: "customer" }),
+  customerController.delete
+);
+
+customerRouter.put(
+  "/:id",
+  auth.autheticate,
+  authPermissionVerify.create({ parameter: "customer" }),
+  customerController.update
+);
+
+customerRouter.get(
+  "/:id",
+  auth.autheticate,
+  authPermissionVerify.create({ parameter: "customer" }),
+  customerController.getById
+);
