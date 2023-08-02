03efe41b7bf90927621b1047acc9dcf59463a4e7
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2023-08-02 16:41:39 -0300

    feat dashboardRouter implements auth and permission

--- src/routes/route/dashBoardRouter.ts
+++ src/routes/route/dashBoardRouter.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import { dashboardController } from "../../controllers/dashBoardController";
+import { auth } from "../../controllers/authController";
+import { authPermissionVerify } from "../../controllers/authPermissionVerify";
 
 export const dashBoardRouter = Router();
 
-dashBoardRouter.get("/", dashboardController.GetAllInfo.bind(dashboardController));
+dashBoardRouter.get(
+  "/",
+  auth.autheticate,
+  authPermissionVerify.view({ parameter: "dashboard" }),
+  dashboardController.GetAllInfo.bind(dashboardController)
+);
--- src/routes/route/orderRouter.ts
+++ src/routes/route/orderRouter.ts
@@ -6,30 +6,35 @@ import { authPermissionVerify } from "../../controllers/authPermissionVerify";
 export const orderRouter = express.Router();
 
 orderRouter.get("/", auth.autheticate, authPermissionVerify.view({ parameter: "order" }), orderController.getAllOrders);
+
 orderRouter.get(
   "/costumer",
   auth.autheticate,
   authPermissionVerify.view({ parameter: "order" }),
   orderController.getCostumerOrders
 );
+
 orderRouter.get(
   "/pending",
   auth.autheticate,
   authPermissionVerify.view({ parameter: "order" }),
   orderController.getOrderPending
 );
+
 orderRouter.post(
   "/",
   auth.autheticate,
   authPermissionVerify.create({ parameter: "order" }),
   orderController.createOrder
 );
+
 orderRouter.delete(
   "/:id",
   auth.autheticate,
   authPermissionVerify.delete({ parameter: "order" }),
   orderController.deleteOrder
 );
+
 orderRouter.put(
   "/:id",
   auth.autheticate,
