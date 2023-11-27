e514d445449706f840540ce144b133fdfe4b8c6f
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2023-11-27 16:07:15 -0300

    Revert "build v1"

--- build/models/RefreshToken.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RefreshTokenModel = void 0;
-var mongoose_1 = require("mongoose");
-var refreshTokenModel = new mongoose_1.Schema({
-    userId: { type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: true },
-    token: { type: String, require: true },
-});
-exports.RefreshTokenModel = (0, mongoose_1.model)("refreshToken", refreshTokenModel);
--- build/routes/route/dashBoardRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dashBoardRouter = void 0;
-var express_1 = require("express");
-var dashBoardController_1 = require("../../controllers/dashBoardController");
-var authController_1 = require("../../controllers/authController");
-var authPermissionVerify_1 = require("../../controllers/authPermissionVerify");
-exports.dashBoardRouter = (0, express_1.Router)();
-exports.dashBoardRouter.get("/", authController_1.auth.autheticate, authPermissionVerify_1.authPermissionVerify.view({ parameter: "dashboard" }), dashBoardController_1.dashboardController.GetAllInfo.bind(dashBoardController_1.dashboardController));
