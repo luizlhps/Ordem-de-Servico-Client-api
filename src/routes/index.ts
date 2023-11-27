4b25e9220fb06f0fb723fd8377a71e2e7ca1b433
Luiz Henrique Pinheiro Silva <luizhenriquepinheirosilva@hotmail.com>
2023-11-27 20:00:10 -0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 export * from "./route/adminRouter";
 export * from "./route/userRouter";
-export * from "./route/financeRouter";
+export * from "./route/financesRouter";
 export * from "./route/statusRouter";
 export * from "./route/customersRouter";
 export * from "./route/serviceRouter";
